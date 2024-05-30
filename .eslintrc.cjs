b69ff34dc021c4a8ade21bba97dbc1f2bc4f2a2c
bjke3 <bjke3@iflytek.com>
2024-05-30 15:39:14 +0800

    feat(cli): merge

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,5 +12,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest'
   },
-
+  /**
+   * "off" 或 0    ==>  关闭规则
+   * "warn" 或 1   ==>  打开的规则作为警告（不影响代码执行）
+   * "error" 或 2  ==>  规则作为一个错误（代码不能执行，界面报错）
+   */
+  rules: {
+    'no-var': 'error' // 要求使用 let 或 const 而不是 var
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// common style
+import '@/styles/common.scss'
+// element css
+import 'element-plus/dist/index.css'
+// element dark css
+import 'element-plus/theme-chalk/dark/css-vars.css'
+// custom element dark css
+import '@/styles/element-dark.scss'
+// custom element css
+import '@/styles/element.scss'
+// main style
+import '@/styles/main.scss'
 
 import App from './App.vue'
 import router from './router'
