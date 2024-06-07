85031a02a3cf450ae222c5d794c0f91e0a80dbee
bjke3 <bjke3@iflytek.com>
2024-06-07 17:59:59 +0800

    feat(cli): add env

---
+++ src/layout/components/EnvNav.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex items-center h-[22px] mr-4 rounded-md px-2 text-xs font-medium border">
+    env
+  </div>
+</template>
+
+<style scoped>
+
+</style>
