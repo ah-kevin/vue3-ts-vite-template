e9c2c4af067f22028f6d9017fd31a7b1bdb6725d
bjke3 <bjke3@iflytek.com>
2024-10-23 23:43:03 +0800

    feat(cli): add

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,10 +1,20 @@
 export default {
-  '*.{js,jsx,ts,tsx}': ['prettier --cache --ignore-unknown  --write', 'eslint --cache --fix'],
+  '*.{js,jsx,ts,tsx}': [
+    'prettier --cache --ignore-unknown  --write',
+    'eslint --cache --fix',
+  ],
   '*.{scss,less,styl,html,vue,css}': [
     'prettier --cache --ignore-unknown --write',
-    // 'stylelint --fix --allow-empty-input',
+    'stylelint --fix --allow-empty-input',
   ],
   '*.md': ['prettier --cache --ignore-unknown --write'],
-  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --cache --write--parser json'],
+  '*.vue': [
+    'prettier --write',
+    'eslint --cache --fix',
+    'stylelint --fix --allow-empty-input',
+  ],
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
+    'prettier --cache --write--parser json',
+  ],
   'package.json': ['prettier --cache --write'],
-}
+};
